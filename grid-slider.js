319b67fa6f7524304a4bd55470efb6a5f3da505f
Alex Dong <team.magiccart@gmail.com>
2023-06-26 16:57:26 +0700

    GridSlider

--- grid-slider.js
+++ grid-slider.js
@@ -1,77 +1,121 @@
-
 class GridSlider extends HTMLElement {
   constructor() {
     super();
-    const $this = this;
+    var $this = this;
     document.addEventListener('DOMContentLoaded', function() {
-	    $this.querySelectorAll('.swiper').forEach((el) => {
-	    	console.log(el);
-		    var swiper = new Swiper(el, {
-		        autoHeight: false,
-		        autoplay:3000,
-		        centeredSlides: false,
-		        delay: 100,
-		        direction: 'horizontal', /* 'horizontal' | 'vertical' */
-		        effect: 'slide', /* 'slide', 'fade', 'cube', 'coverflow', 'flip' or 'creative' */
-		        grid: {
-		            rows: 2,
-		            fill: "row",
-		        },
-		        init: true,
-		        loop: true,
-		        slidesPerView: 1, /* mobile First */
-		        spaceBetween: 15,
-		        speed: 400,
-		        breakpoints: {
-		            // when window width is >= 320px
-		            320: {
-		                slidesPerView: 2,
-		                spaceBetween: 20
-		            },
-		            // when window width is >= 480px
-		            480: {
-		                slidesPerView: 3,
-		                spaceBetween: 30
-		            },
-		            // when window width is >= 640px
-		            640: {
-		                slidesPerView: 4,
-		                spaceBetween: 40
+	    $this.initSlider();
+	  });
+  }
+
+		uniqid(length) {
+			length = length || 10;
+            var result       	   = '',
+            	characters 	   = 'abcdefghijklmnopqrstuvwxyz0123456789',
+            	charactersLength = characters.length;
+            for ( var i = 0; i < length; i++ ) {
+            	result += characters.charAt(Math.floor(Math.random() * charactersLength));
+           	}
+           	return result;
+		}
+
+	  initSlider() {
+	  		var $head = document.querySelector('head'),
+			  		$body = document.querySelector('body'),
+						isRTL = $body.classList.contains('rtl');
+						var options = this.datasetToObject(this.dataset);
+					    	this.querySelectorAll('.swiper').forEach((element) => {
+					    	var selector = 'grid-slider-' + this.uniqid(),
+					    			styleId  = selector;
+					    	element.classList.add(selector);
+					    	element.classList.add('grid-init');
+					    	selector = '.' + selector;
+		            if(isRTL){
+		                element.setAttribute('dir', 'rtl');
 		            }
-		        }
-		    });
-	    });
-    })
+		            if(options.slidesPerView){
+									this.sliderRender(element);
+									return;
+		            }
+		            var responsive 	= this.getBreakpoints(options);
+								if(responsive == undefined) return;
+           			if(iClass === undefined){
+									element.querySelectorAll('.swiper-wrapper > *').forEach((el) => {
+									    el.classList.add('alo-item');
+									});
+		              var iClass = '.alo-item';
+		            }
+            		var classes	= selector + ' '+ iClass,
+	            			padding = ((options || {}).spaceBetween === void 0) ? 0 : options.spaceBetween/2,
+	            			style 		= '';
+								var length = Object.keys(responsive).length;
+								Object.entries(responsive).forEach(item => {
+									  var key  = parseInt(item[0]), 
+                      	value = item[1];
+									var col = 0, maxWith = 0, minWith = 0;
+									Object.entries(value).forEach(entry => {
+									  	var size  = parseInt(entry[0]), 
+                      		num = entry[1];
+                      minWith = size + 1; col = num;
+									});
+									if(key+2<length){
+										Object.entries(responsive[key+1]).forEach(entry => {
+											  	var size  = parseInt(entry[0]), 
+		                      		num = entry[1];
+		                      maxWith = size; col = num;
+										});
+										style += ' @media (min-width: '+minWith+'px) and (max-width: '+maxWith+'px)';
+									} else { 
+										if(key+2 == length) return; // don't use key = length - 1;
+										Object.entries(responsive[key]).forEach(entry => {
+											  	var size  = parseInt(entry[0]), 
+		                      		num = entry[1];
+		                      maxWith = size; col = num;
+										});
+										style += ' @media (min-width: '+maxWith+'px)';
+									}
+									var clearRtl = classes + ':nth-child('+col+'n+1){clear: left}';
+                  clearRtl 	+= ' .rtl ' + classes+':nth-child('+col+'n+1){clear: right}';
+									style += ' {'+selector + '{margin: 0 -'+padding+'px}'+classes+'{padding: 0 '+padding+'px; box-sizing: border-box; width: calc(100% / ' + col + ')} ' + clearRtl + '}';
+								});	
+		           	$head.innerHTML += '<style type="text/css" id="' + styleId + '" >'+style+'</style>';
+			    });
 
-  }
+	    }
 
-  toggleResetButton() {
-    const resetIsHidden = this.resetButton.classList.contains('hidden');
-    if (this.input.value.length > 0 && resetIsHidden) {
-      this.resetButton.classList.remove('hidden');
-    } else if (this.input.value.length === 0 && !resetIsHidden) {
-      this.resetButton.classList.add('hidden');
-    }
-  }
+	    datasetToObject(dataset) {
+	    	return JSON.parse(JSON.stringify(dataset), (key, value) => {
+					if(value === "null") return null;
+					if(value === "true") return true;
+					if(value === "false") return false;
+					if(!isNaN(value)) return Number(value);
+				  try {
+				       return JSON.parse(value);
+				    } catch (e) {
+				      return value;
+				  }
+				});
+	    }
 
-  onChange() {
-    this.toggleResetButton();
-  }
+	    getBreakpoints(options) {
+	    	if(!options.breakpoints) return;
+				var gridResponsive = [];
+				Object.entries(options.breakpoints).forEach(entry => {
+						var size  = entry[0], 
+								value = entry[1],
+								breakpoint = {};
+								breakpoint[size] = parseInt(value.slidesPerView);
+								gridResponsive.push(breakpoint);
+						});
+				return gridResponsive;
+	    }
 
-  shouldResetForm() {
-    return !document.querySelector('[aria-selected="true"] a');
-  }
-
-  onFormReset(event) {
-    // Prevent default so the form reset doesn't set the value gotten from the url on page load
-    event.preventDefault();
-    // Don't reset if the user has selected an element on the predictive search dropdown
-    if (this.shouldResetForm()) {
-      this.input.value = '';
-      this.input.focus();
-      this.toggleResetButton();
-    }
-  }
+	    sliderRender(element) {
+	    	if(element.classList.contains('swiper-initialized')){
+	    		return;
+	    	}
+				var options = this.datasetToObject(this.dataset);
+				var swiper = new Swiper(element, options);
+	    }
 }
 
 customElements.define('grid-slider', GridSlider);
