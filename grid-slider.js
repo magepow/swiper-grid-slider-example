5b15d263e049c7ab8351b7b9a1baa9053353d8cf
Alex Dong <team.magiccart@gmail.com>
2023-06-27 08:48:24 +0700

    navigation and pagination

--- grid-slider.js
+++ grid-slider.js
@@ -2,7 +2,10 @@ class GridSlider extends HTMLElement {
   constructor() {
     super();
     var $this = this;
-    document.addEventListener('DOMContentLoaded', function() {
+    document.addEventListener('DOMContentLoaded', function(event) {
+	    $this.initSlider();
+	  });
+	  this.addEventListener("gridAjax", function(event){
 	    $this.initSlider();
 	  });
   }
@@ -21,64 +24,66 @@ class GridSlider extends HTMLElement {
 	  initSlider() {
 	  		var $head = document.querySelector('head'),
 			  		$body = document.querySelector('body'),
-						isRTL = $body.classList.contains('rtl');
-						var options = this.datasetToObject(this.dataset);
-					    	this.querySelectorAll('.swiper').forEach((element) => {
-					    	var selector = 'grid-slider-' + this.uniqid(),
-					    			styleId  = selector;
-					    	element.classList.add(selector);
-					    	element.classList.add('grid-init');
-					    	selector = '.' + selector;
-		            if(isRTL){
-		                element.setAttribute('dir', 'rtl');
-		            }
-		            if(options.slidesPerView){
-									this.sliderRender(element);
-									return;
-		            }
-		            var responsive 	= this.getBreakpoints(options);
-								if(responsive == undefined) return;
-           			if(iClass === undefined){
-									element.querySelectorAll('.swiper-wrapper > *').forEach((el) => {
-									    el.classList.add('alo-item');
-									});
-		              var iClass = '.alo-item';
-		            }
-            		var classes	= selector + ' '+ iClass,
-	            			padding = ((options || {}).spaceBetween === void 0) ? 0 : options.spaceBetween/2,
-	            			style 		= '';
-								var length = Object.keys(responsive).length;
-								Object.entries(responsive).forEach(item => {
-									  var key  = parseInt(item[0]), 
-                      	value = item[1];
-									var col = 0, maxWith = 0, minWith = 0;
-									Object.entries(value).forEach(entry => {
+						isRTL = $body.classList.contains('rtl'),
+						options = this.datasetToObject(this.dataset);
+				console.log(options);
+			  this.querySelectorAll('.swiper').forEach((element) => {
+			    	if(element.classList.contains('grid-init')) return;
+			    	var selector = 'grid-slider-' + this.uniqid(),
+			    			styleId  = selector;
+			    	element.classList.add(selector);
+			    	element.classList.add('grid-init');
+			    	selector = '.' + selector;
+            if(isRTL){
+                element.setAttribute('dir', 'rtl');
+            }
+            if(options.slidesPerView){
+							this.sliderRender(element);
+							return;
+            }
+            var responsive 	= this.getBreakpoints(options);
+						if(responsive == undefined) return;
+       			if(iClass === undefined){
+							element.querySelectorAll('.swiper-wrapper > *').forEach((el) => {
+							    el.classList.add('alo-item');
+							});
+              var iClass = '.alo-item';
+            }
+        		var classes	= selector + ' '+ iClass,
+          			padding = ((options || {}).spaceBetween === void 0) ? 0 : options.spaceBetween/2,
+          			style 		= '';
+						var length = Object.keys(responsive).length;
+						Object.entries(responsive).forEach(item => {
+							  var key  = parseInt(item[0]), 
+                  	value = item[1];
+							var col = 0, maxWith = 0, minWith = 0;
+							Object.entries(value).forEach(entry => {
+							  	var size  = parseInt(entry[0]), 
+                  		num = entry[1];
+                  minWith = size + 1; col = num;
+							});
+							if(key+2<length){
+								Object.entries(responsive[key+1]).forEach(entry => {
 									  	var size  = parseInt(entry[0]), 
                       		num = entry[1];
-                      minWith = size + 1; col = num;
-									});
-									if(key+2<length){
-										Object.entries(responsive[key+1]).forEach(entry => {
-											  	var size  = parseInt(entry[0]), 
-		                      		num = entry[1];
-		                      maxWith = size; col = num;
-										});
-										style += ' @media (min-width: '+minWith+'px) and (max-width: '+maxWith+'px)';
-									} else { 
-										if(key+2 == length) return; // don't use key = length - 1;
-										Object.entries(responsive[key]).forEach(entry => {
-											  	var size  = parseInt(entry[0]), 
-		                      		num = entry[1];
-		                      maxWith = size; col = num;
-										});
-										style += ' @media (min-width: '+maxWith+'px)';
-									}
-									var clearRtl = classes + ':nth-child('+col+'n+1){clear: left}';
-                  clearRtl 	+= ' .rtl ' + classes+':nth-child('+col+'n+1){clear: right}';
-									style += ' {'+selector + '{margin: 0 -'+padding+'px}'+classes+'{padding: 0 '+padding+'px; box-sizing: border-box; width: calc(100% / ' + col + ')} ' + clearRtl + '}';
-								});	
-		           	$head.innerHTML += '<style type="text/css" id="' + styleId + '" >'+style+'</style>';
-			    });
+                      maxWith = size; col = num;
+								});
+								style += ' @media (min-width: '+minWith+'px) and (max-width: '+maxWith+'px)';
+							} else { 
+								if(key+2 == length) return; // don't use key = length - 1;
+								Object.entries(responsive[key]).forEach(entry => {
+									  	var size  = parseInt(entry[0]), 
+                      		num = entry[1];
+                      maxWith = size; col = num;
+								});
+								style += ' @media (min-width: '+maxWith+'px)';
+							}
+							var clearRtl = classes + ':nth-child('+col+'n+1){clear: left}';
+              clearRtl 	+= ' .rtl ' + classes+':nth-child('+col+'n+1){clear: right}';
+							style += ' {'+selector + '{margin: 0 -'+padding+'px}'+classes+'{padding: 0 '+padding+'px; box-sizing: border-box; width: calc(100% / ' + col + ')} ' + clearRtl + '}';
+						});	
+           	$head.innerHTML += '<style type="text/css" id="' + styleId + '" >'+style+'</style>';
+	    	});
 
 	    }
 
